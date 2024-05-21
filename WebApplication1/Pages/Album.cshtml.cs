b3c9fe14435aaec5ace99e306723cee113f3c95b
LeeYi-user <khes60122@gmail.com>
2024-05-21 21:37:12 +0800

    add album

---
+++ WebApplication1/Pages/Album.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
+
+namespace WebApplication1.Pages
+{
+    public class AlbumModel : PageModel
+    {
+        [BindProperty]
+        public DataTable dataTable { get; set; }
+
+        public void OnGet()
+        {
+            var connection = new SqliteConnection(@"data source=Databases\MyDB.db");
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = @"SELECT * FROM [Picture]";
+            var reader = command.ExecuteReader();
+            dataTable = new DataTable();
+            dataTable.Load(reader);
+            reader.Close();
+            connection.Close();
+        }
+    }
+}
