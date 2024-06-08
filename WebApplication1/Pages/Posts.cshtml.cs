308cc926cbcdad681491c7c2b0071a48a5782ae2
LeeYi-user <khes60122@gmail.com>
2024-06-09 02:29:59 +0800

    finish posts 1

--- WebApplication1/Pages/Posts.cshtml.cs
+++ WebApplication1/Pages/Posts.cshtml.cs
@@ -1,12 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
+using System.Data;
 
 namespace WebApplication1.Pages
 {
     public class PostsModel : PageModel
     {
+        [BindProperty]
+        public DataTable dataTable { get; set; }
+
         public void OnGet()
         {
+            var connection = new SqliteConnection(@"data source=Databases\MyDB.db");
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = @"SELECT * FROM [Posts]";
+            var reader = command.ExecuteReader();
+            dataTable = new DataTable();
+            dataTable.Load(reader);
+            reader.Close();
+            connection.Close();
+        }
+
+        public void OnPostUpdate(string id)
+        {
+            Response.Redirect("PostEdit");
+        }
+
+        public void OnPostDelete(string id)
+        {
+
         }
     }
 }
