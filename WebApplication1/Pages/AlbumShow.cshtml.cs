bb365673f886224195ab71975a6a00b19fb3f2ae
LeeYi-user <khes60122@gmail.com>
2024-06-10 23:13:09 +0800

    add slideshow

---
+++ WebApplication1/Pages/AlbumShow.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace WebApplication1.Pages
+{
+    public class AlbumShowModel : PageModel
+    {
+        [BindProperty]
+        public DataTable dataTable { get; set; }
+
+        public void OnGet()
+        {
+            var connection = new SqliteConnection(@"data source=Databases\MyDB.db");
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = @"SELECT * FROM [Picture2]";
+            var reader = command.ExecuteReader();
+            dataTable = new DataTable();
+            dataTable.Load(reader);
+            reader.Close();
+            connection.Close();
+        }
+    }
+}
